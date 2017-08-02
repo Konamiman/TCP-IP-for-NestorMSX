72dbf1214297211ba14361d077c2de7314b88fca
Néstor Soriano Vilchez <konamiman@stackoverflow.com>
2017-08-02 22:41:21 +0900

    Implemented UDP support.

--- TcpipUnapiPlugin/TcpipUnapiPlugin.cs
+++ TcpipUnapiPlugin/TcpipUnapiPlugin.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Net;
 using System.Net.NetworkInformation;
@@ -20,6 +21,7 @@ namespace Konamiman.NestorMSX.Plugins.TcpipUnapi
         private const int EXTBIO = 0xFFCA;
         private const int ARG = 0xF847;
         private const string SpecIdentifier = "TCP/IP";
+        private const int MaxUdpConnections = 4;
 
         private static TcpipUnapiPlugin Instance;
 
@@ -29,6 +31,7 @@ namespace Konamiman.NestorMSX.Plugins.TcpipUnapi
         private readonly NetworkInterface networkInterface;
         private readonly UnicastIPAddressInformation ipInfo;
         private readonly bool dnsServersAvailable;
+        private readonly int mtu;
 
         public static TcpipUnapiPlugin GetInstance(PluginContext context, IDictionary<string, object> pluginConfig)
         {
@@ -79,7 +82,8 @@ namespace Konamiman.NestorMSX.Plugins.TcpipUnapi
             }
 
             dnsServersAvailable = networkInterface.GetIPProperties().DnsAddresses.Any();
-
+            mtu = (short)Math.Min(32767, networkInterface.GetIPProperties().GetIPv4Properties().Mtu);
+            
             InitRoutinesArray();
         }
 
@@ -140,6 +144,13 @@ namespace Konamiman.NestorMSX.Plugins.TcpipUnapi
         private void HandleEntryPointCall()
         {
             var functionNumber = cpu.Registers.A;
+
+            if (functionNumber == 29)
+            {
+                cpu.Registers.IFF1 = 1;
+                cpu.Registers.IFF1 = 2;
+            }
+
             if (functionNumber < Routines.Length && Routines[functionNumber] != null)
                 cpu.Registers.A = Routines[functionNumber]();
             else
@@ -160,18 +171,28 @@ namespace Konamiman.NestorMSX.Plugins.TcpipUnapi
                 null, //TCPIP_SEND_ECHO
                 null, //TCPIP_RCV_ECHO
                 TCPIP_DNS_Q,
-                TCPIP_DNS_S
+                TCPIP_DNS_S,
+                TCPIP_UDP_OPEN,
+                TCPIP_UDP_CLOSE,
+                TCPIP_UDP_STATE,
+                TCPIP_UDP_SEND,
+                TCPIP_UDP_RCV
             };
         }
 
         private const int ERR_OK = 0;
         private const int ERR_NOT_IMP = 1;
         private const int ERR_NO_NETWORK = 2;
+        private const int ERR_NO_DATA = 3;
         private const int ERR_INV_PAR = 4;
         private const int ERR_QUERY_EXISTS = 5;
         private const int ERR_INV_IP = 6;
         private const int ERR_NO_DNS = 7;
         private const int ERR_DNS = 8;
+        private const int ERR_NO_FREE_CONN = 9;
+        private const int ERR_CONN_EXISTS = 10;
+        private const int ERR_NO_CONN = 11;
+        private const int ERR_LARGE_DGRAM = 14;
 
         private byte UNAPI_GET_INFO()
         {
@@ -197,7 +218,7 @@ namespace Konamiman.NestorMSX.Plugins.TcpipUnapi
                         0 << 7 | // 7: Explicitly set the PUSH bit when sending TCP data
                         0 << 8 | // 8: Send data to a TCP connection before the ESTABLISHED state is reached
                         0 << 9 | // 9: Flush the output buffer of a TCP connection
-                        0 << 10 | // 10: Open UDP connections
+                        1 << 10 | // 10: Open UDP connections
                         0 << 11 | // 11: Open raw IP connections
                         0 << 12 | // 12: Explicitly set the TTL and TOS for outgoing datagrams
                         0 << 13 | // 13: Explicitly set the automatic reply to PINGs on or off
@@ -228,15 +249,16 @@ namespace Konamiman.NestorMSX.Plugins.TcpipUnapi
                     break;
 
                 case 2:
-                    cpu.Registers.BC = 0x0404;
-                    cpu.Registers.DE = 0x0404;
+                    cpu.Registers.B = 0;
+                    cpu.Registers.C = MaxUdpConnections;
+                    cpu.Registers.D = 0;
+                    cpu.Registers.E = (byte)udpConnections.Count(c => c == null);
                     cpu.Registers.HL = 0;
                     break;
 
                 case 3:
-                    var mtu = (short)Math.Min(32767, networkInterface.GetIPProperties().GetIPv4Properties().Mtu);
-                    cpu.Registers.HL = mtu;
-                    cpu.Registers.DE = mtu;
+                    cpu.Registers.HL = mtu.ToShort();
+                    cpu.Registers.DE = mtu.ToShort();
                     break;
             }
 
@@ -448,6 +470,200 @@ namespace Konamiman.NestorMSX.Plugins.TcpipUnapi
             }
 
             cpu.Registers.B = 0;
+            return ERR_OK;
+        }
+
+        private class UdpDatagram
+        {
+            public IPEndPoint RemoteEndpoint { get; set; }
+            public byte[] Data { get; set; } 
+        }
+
+        private class UdpConnection
+        {
+            public int Port { get; set; }
+            public UdpClient Client { get; set; }
+            public bool IsTransient { get; set; }
+            private UdpDatagram BufferedReceivedDatagram { get; set; }
+            
+            public bool HasIncomingDataAvailable =>
+                BufferedReceivedDatagram != null || Client.Available > 0;
+
+            public ushort SizeOfLastReceivedDatagram
+            {
+                get
+                {
+                    if (BufferedReceivedDatagram != null)
+                        return (ushort)BufferedReceivedDatagram.Data.Length;
+
+                    BufferedReceivedDatagram = GetDatagramFromNetwork();
+                    return (ushort)(BufferedReceivedDatagram?.Data?.Length ?? 0);
+                }
+            }
+
+            public UdpDatagram GetDatagram()
+            {
+                if (BufferedReceivedDatagram != null)
+                {
+                    var datagram = BufferedReceivedDatagram;
+                    BufferedReceivedDatagram = null;
+                    return datagram;
+                }
+
+                return GetDatagramFromNetwork();
+            }
+
+            private UdpDatagram GetDatagramFromNetwork()
+            {
+                if (Client.Available == 0)
+                    return null;
+
+                IPEndPoint remoteEndpoint = null;
+                var data = Client.Receive(ref remoteEndpoint);
+                return new UdpDatagram {Data = data, RemoteEndpoint = remoteEndpoint};
+            }
+        }
+
+        private readonly UdpConnection[] udpConnections = new UdpConnection[MaxUdpConnections];
+
+        private byte TCPIP_UDP_OPEN()
+        {
+            ushort? port = cpu.Registers.HL.ToUShort();
+
+            if (port == 0xFFFF)
+                port = null;
+
+            if (port == 0 || port >= 0xFFF0 || cpu.Registers.B > 1)
+                return ERR_INV_PAR;
+
+            if (udpConnections.All(c => c != null))
+                return ERR_NO_FREE_CONN;
+
+            if(port != null && udpConnections.Any(c => c?.Port == port.Value))
+                return ERR_CONN_EXISTS;
+
+            var client = port == null ? new UdpClient() : new UdpClient((int)port);
+            for (var i = 0; i < MaxUdpConnections; i++)
+            {
+                if (udpConnections[i] == null)
+                {
+                    udpConnections[i] = new UdpConnection
+                    {
+                        Client = client,
+                        IsTransient = cpu.Registers.B == 0,
+                        Port = ((IPEndPoint)client.Client.LocalEndPoint).Port
+                    };
+                    cpu.Registers.B = (byte)(i+1);
+                    break;
+                }
+            }
+
+            return ERR_OK;
+        }
+
+        private UdpConnection GetUdpConnection(byte connectionNumber)
+        {
+            if (connectionNumber == 0 || connectionNumber >= (MaxUdpConnections + 1))
+                return null;
+
+            return udpConnections[connectionNumber - 1];
+        }
+
+        private byte TCPIP_UDP_CLOSE()
+        {
+            var connection = cpu.Registers.B - 1;
+
+            if (connection == -1)
+            {
+                for (var i = 0; i < MaxUdpConnections; i++)
+                {
+                    if (udpConnections[i]?.IsTransient == true)
+                    {
+                        udpConnections[i].Client.Close();
+                        udpConnections[i] = null;
+                    }
+                }
+                return ERR_OK;
+            }
+
+            if (connection >= MaxUdpConnections || udpConnections[connection] == null)
+                return ERR_NO_CONN;
+
+            udpConnections[connection].Client.Close();
+            udpConnections[connection] = null;
+            return ERR_OK;
+        }
+
+        private byte TCPIP_UDP_STATE()
+        {
+            var connection = GetUdpConnection(cpu.Registers.B);
+            if (connection == null)
+                return ERR_NO_CONN;
+
+            cpu.Registers.HL = connection.Port.ToShort();
+            cpu.Registers.B = connection.HasIncomingDataAvailable ? (byte)0 : (byte)1;
+            cpu.Registers.DE = connection.SizeOfLastReceivedDatagram.ToShort();
+
+            return ERR_OK;
+        }
+
+        private byte TCPIP_UDP_SEND()
+        {
+            var connection = GetUdpConnection(cpu.Registers.B);
+            if (connection == null)
+                return ERR_NO_CONN;
+
+            if (NoNetworkAvailable())
+                return ERR_NO_NETWORK;
+
+            var paramsAddress = cpu.Registers.DE;
+            var destIp = new IPAddress(new byte[] {slots[paramsAddress], slots[paramsAddress+1], slots[paramsAddress+2], slots[paramsAddress+3]});
+            var destPort = NumberUtils.CreateUshort(slots[paramsAddress + 4], slots[paramsAddress + 5]);
+            var dataSize = NumberUtils.CreateUshort(slots[paramsAddress + 6], slots[paramsAddress + 7]);
+
+            if (dataSize > mtu - 28)
+                return ERR_LARGE_DGRAM;
+
+            if (dataSize == 0)
+                return ERR_OK;
+
+            var data = new byte[dataSize];
+            var dataAddress = cpu.Registers.HL;
+            for (var i = 0; i < dataSize; i++)
+                data[i] = slots[dataAddress + i];
+
+            var remoteEndpoint = new IPEndPoint(destIp, destPort);
+            connection.Client.Send(data, dataSize, remoteEndpoint);
+
+            return ERR_OK;
+        }
+
+        private byte TCPIP_UDP_RCV()
+        {
+            var connection = GetUdpConnection(cpu.Registers.B);
+            if (connection == null)
+                return ERR_NO_CONN;
+
+            if (!connection.HasIncomingDataAvailable)
+                return ERR_NO_DATA;
+
+            var datagram = connection.GetDatagram();
+            var maxDataSize = cpu.Registers.DE;
+            var addressForData = cpu.Registers.HL;
+
+            var dataSizeToCopy = Math.Min(maxDataSize, datagram.Data.Length);
+            for (var i = 0; i < dataSizeToCopy; i++)
+                slots[addressForData + i] = datagram.Data[i];
+
+            var remoteIpBytes = datagram.RemoteEndpoint.Address.GetAddressBytes();
+            cpu.Registers.L = remoteIpBytes[0];
+            cpu.Registers.H = remoteIpBytes[1];
+            cpu.Registers.E = remoteIpBytes[2];
+            cpu.Registers.D = remoteIpBytes[3];
+
+            cpu.Registers.IX = datagram.RemoteEndpoint.Port.ToShort();
+            cpu.Registers.BC = dataSizeToCopy.ToShort();
+
             return ERR_OK;
         }
     }
